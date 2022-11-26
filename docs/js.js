427fc5b0f7194b93fd82a6cd697b36811bd12f76
knockjoy <87211271+Knockjoy@users.noreply.github.com>
2022-11-26 18:09:51 +0900

    Add files via upload

--- docs/js.js
+++ docs/js.js
@@ -1,3 +1,28 @@
-function check_cookie(){
-    console.log(document.cookie)
+// import * as Vue from "https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js";
+
+Vue.component("sample", {
+    template: `<p>hello</p>`
+});
+
+function check_cookie() {
+    var cookies = document.cookie
+    return cookies
 }
+function start() {
+    var screen = document.getElementById("screen");
+    screen.innerHTML = '<iframe src="index2.html"></iframe>'
+}
+
+async function get_html() {
+    const result = await fetch('index2.html', {
+        method: "GET"
+      }).then(function(response) {
+        return response.text();
+      }).then(function(data) {
+          const a=new DOMParser().parseFromString(data, "text/html")
+      console.log(a)
+      });
+}
+
+var cookies = check_cookie()
+
