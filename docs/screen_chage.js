725f6bbd7b879fada33d8f71786771327b6c690c
knockjoy <87211271+Knockjoy@users.noreply.github.com>
2022-12-09 18:14:59 +0900

    Add files via upload

--- docs/screen_chage.js
+++ docs/screen_chage.js
@@ -1,20 +1,47 @@
+var datas;
+
 function chage_data(id, data) {
     var element = document.getElementById(id)
     element.innerHTML = data
 }
 
-function chage_data_to_html(id,path){
-    chage_data(id,get_html(path))
+async function chage_data_to_html(id,path){
+    chage_data(id,await get_html(path))
 }
-
 async function get_html(path) {
-    const result = await fetch(path, {
+    const result =await fetch(path, {
         method: "GET"
     }).then(function (response) {
         return response.text();
     }).then(function (data) {
         const a = new DOMParser().parseFromString(data, "text/html")
-        console.log(a)
-        return a
-    });
+        datas=a.body.innerHTML;
+        return datas
+    });}
+
+async function sample2(){
+    chage_data("screen", await sample1())
 }
+
+// var datas;
+// async function sample1(path) {
+//     console.log("path:"+path)
+//     const result =await fetch(path, {
+//         method: "GET"
+//     }).then(function (response) {
+//         return response.text();
+//     }).then(function (data) {
+//         const a = new DOMParser().parseFromString(data, "text/html")
+//         console.log(a)
+//         datas=a.body.innerHTML;
+
+//     console.log(datas)
+//         return datas
+//     });}
+
+// async function sample2(){
+//     await sample1("title.html")
+//     console.log(datas)
+//     chage_data("screen", datas)
+// }
+// sample2()
