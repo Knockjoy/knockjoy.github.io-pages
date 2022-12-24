6f7ae456579653d7857a77c2e4a78975974e9c7d
knockjoy <87211271+Knockjoy@users.noreply.github.com>
2022-12-24 10:10:26 +0900

    Add files via upload

--- docs/js.js
+++ docs/js.js
@@ -1,19 +1,123 @@
 // import * as Vue from "https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js";
-
-window.onload = function () {
-    chage_data_to_html("screen","title.html")
+var loading;
+window.onload = async function () {
+    await get_html("loading.html");
+    console.log(datas);
+    loading = datas;
+    change_data_to_html("screen", "title.html");
 }
 
+async function addlistener() {
+    const input_file = document.getElementById("import_file");
+    console.log(input_file)
+    input_file.addEventListener("input", update_file)
+}
 
 function check_cookie() {
     var cookies = document.cookie
     return cookies
 }
-function start() {
+async function start() {
+    await change_data("screen", loading);
+    await change_data_to_html("screen", "import.html");
+    await addlistener();
+}
+
+async function settings() {
+    await change_data("screen", loading);
+    await change_data_to_html("screen", "settings.html");
+}
+
+async function home() {
+    await change_data("screen", loading);
+    await change_data_to_html("screen", "title.html");
+}
+function option(import_data) {
+    console.log(import_data)
+    if (import_file) {
+        
+        //ファイルでimport
+    } else {
+        if (import_id) {
+            import_id.match('/^https:/')
+            //id,url でimport
+        } else {
+
+            //error
+        }
+    }
+    change_data_to_html("screen", "start_option.html")
+}
+
+function next() {
+    check_id()
+    console.log(`file:${import_file}\nid:${import_id}`)
+    if (import_file) {
+        if (import_id) {
+            change_data("error", "入力はどちらかにしてください。")
+            //true が重複
+        } else {
+            var file=document.getElementById("import_file").value;
+            option(file)
+            //ok import fileでimport
+        }
+    } else {
+        if (import_id) {
+            var id= document.getElementById("sheets").value;
+            option(id)
+            //ok import id でimport
+        } else {
+            change_data("error", "なにも入力されていません")
+            //どちらもfales
+        }
+    }
+    // change_data_to_html("screen", "start_option.html");
 
 }
 
+var import_file = false;
+var import_id = false;
+async function check_id() {
+    var id_box = document.getElementById("sheets");
+    if (id_box.value != "") {
+        import_id = true;
+    } else {
+        import_id = false;
+    }
+}
 
+function update_file() {
+    var file = document.getElementById("import_file").value;
+    var file_value = document.getElementById("file_value");
+    console.log(file);
+    if (file != "") {
+        var regex = /\\|\\/;
+        var array = file.split(regex);
+        file_value.innerHTML = array[array.length - 1];
+        import_file = true;
+    } else {
+        file_value.innerHTML = "未選択";
+        import_file = false;
+    }
+}
 
-var cookies = check_cookie()
+function arrange_url(){
+
+}
+
+async function get_quiz(id){
+    const path="https://script.google.com/macros/s/AKfycbw5n62U2I0PiFyViFqz-gv9T4sBfv71mCMRkGNdm2i7sqVfvfq-Id2Wpk7V6pF3MEih/exec";
+    const result=await fetch(path,{
+        method:"cors",
+        headers:{
+            "Access-Control-Allow-Origin":"https://script.google.com",
+            "Content-Type":"application/json; charset=utf-8"
+        },
+        body:JSON.stringify({
+            id:id
+        })
+    })
+}
+
+var cookies = check_cookie();
 
--- docs/screen_chage.js
+++ docs/screen_chage.js
@@ -1,15 +1,44 @@
 var datas;
 
-function chage_data(id, data) {
+var pages_cash=[]
+
+function create_cash(data_path,data){
+pages_cash.push({"path":data_path,"data":data})
+}
+
+function output_cash(path){
+var result="undefind";
+    pages_cash.forEach(function(value){
+    if(value.path==path){
+        result= value.data;
+    }
+})
+return result;
+}
+
+function change_data(id, data) {
     var element = document.getElementById(id);
     element.innerHTML = data;
 }
 
-async function chage_data_to_html(id,path){
-    await get_html(path);
-    chage_data(id,datas);
+
+
+async function change_data_to_html(id,path){
+    var cash_data=output_cash(path);
+    if(cash_data=="undefind"){
+        await get_html(path);
+        change_data(id,datas);
+        create_cash(path,datas);
+        console.log("loaded");
+        
+    }else{
+        change_data(id,cash_data);
+        console.log("used cash");
+    }
+
 }
 async function get_html(path) {
+// try{
     const result =await fetch(path, {
         method: "GET"
     }).then(function (response) {
@@ -17,31 +46,14 @@ async function get_html(path) {
     }).then(function (data) {
         const a = new DOMParser().parseFromString(data, "text/html");
         datas=a.body.innerHTML;
-    });}
-
-async function sample2(){
-    chage_data("screen", await sample1());
+    });
+// }catch{
+//     change_data("screen",`<div class = "samp1" ></div><div class="fream_title">
+//     <span class="title title_border">予期せぬエラーが発生しました。再度お試しください。</span>
+// </div>`)
+// }
 }
 
-// var datas;
-// async function sample1(path) {
-//     console.log("path:"+path)
-//     const result =await fetch(path, {
-//         method: "GET"
-//     }).then(function (response) {
-//         return response.text();
-//     }).then(function (data) {
-//         const a = new DOMParser().parseFromString(data, "text/html")
-//         console.log(a)
-//         datas=a.body.innerHTML;
-
-//     console.log(datas)
-//         return datas
-//     });}
-
-// async function sample2(){
-//     await sample1("title.html")
-//     console.log(datas)
-//     chage_data("screen", datas)
-// }
-// sample2()
+async function sample2(){
+    change_data("screen", await sample1());
+}
